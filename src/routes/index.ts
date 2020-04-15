b8bdb54013ab9638ae1409cc8f7088e8da7cf588
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-04-15 20:05:55 -0300

    criando conceito de model da rota appointment

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,8 @@
 import { Router } from 'express';
+import appointments from './appointments.route';
 
 const routes = Router();
 
-routes.get('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-  return response.json(user);
-});
+routes.use('/appointments', appointments);
 
 export default routes;
